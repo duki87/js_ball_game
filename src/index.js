5e41734c63a8f1957c2d24b40e364a3ba2c8b931
Dusan <43047297+duki87@users.noreply.github.com>
2020-04-11 23:03:07 +0200

    next: handling keyboard input

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import "./styles.css";
+import Paddle from "./paddle";
+let canvas = document.getElementById("gameScreen");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let ctx = canvas.getContext("2d");
+
+ctx.clearRect(0, 0, 800, 600);
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  //console.log(timestamp);
+  lastTime = timestamp;
+  ctx.clearRect(0, 0, 800, 600);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
