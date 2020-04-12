3208ecc638dfc9d2351147111acc8abdcb6bcb6a
Dusan <43047297+duki87@users.noreply.github.com>
2020-04-12 23:13:07 +0200

    next: collision detection

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 import "./styles.css";
-import Paddle from "./paddle";
+import Game from "./game";
+
 let canvas = document.getElementById("gameScreen");
-
 let ctx = canvas.getContext("2d");
-
 ctx.clearRect(0, 0, 800, 600);
 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
-let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-paddle.draw(ctx);
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+game.start();
 
 let lastTime = 0;
 
 function gameLoop(timestamp) {
   let deltaTime = timestamp - lastTime;
-  //console.log(timestamp);
   lastTime = timestamp;
-  ctx.clearRect(0, 0, 800, 600);
-  paddle.update(deltaTime);
-  paddle.draw(ctx);
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  game.update(deltaTime);
+  game.draw(ctx);
   requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+requestAnimationFrame(gameLoop);
--- src/paddle.js
+++ src/paddle.js
@@ -1,11 +1,15 @@
 export default class Paddle {
-  constructor(gameWidth, gameHeight) {
+  constructor(game) {
     this.width = 150;
     this.height = 30;
+    this.gameWidth = game.gameWidth;
+    this.gameHeight = game.gameHeight;
+    this.maxSpeed = 7;
+    this.speed = 0;
 
     this.position = {
-      x: gameWidth / 2 - this.width / 2,
-      y: gameHeight - this.height - 10
+      x: this.gameWidth / 2 - this.width / 2,
+      y: this.gameHeight - this.height - 10
     };
   }
 
@@ -15,7 +19,24 @@ export default class Paddle {
   }
 
   update(deltaTime) {
-    if (!deltaTime) return;
-    this.position.x += 5 / deltaTime;
+    this.position.x += this.speed;
+    if (this.position.x < 0) {
+      this.position.x = 0;
+    }
+    if (this.position.x + this.width > this.gameWidth) {
+      this.position.x = this.gameWidth - this.width;
+    }
+  }
+
+  moveLeft() {
+    this.speed = -this.maxSpeed;
+  }
+
+  moveRight() {
+    this.speed = this.maxSpeed;
+  }
+
+  stop() {
+    this.speed = 0;
   }
 }
