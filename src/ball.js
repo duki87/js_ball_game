ec4107069f2cb17a927419c4743998dc9f26091d
Dusan <43047297+duki87@users.noreply.github.com>
2020-04-14 22:46:59 +0200

    next: loading bricks

--- src/ball.js
+++ src/ball.js
@@ -1,5 +1,6 @@
 export default class Ball {
   constructor(game) {
+    this.game = game;
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
     this.image = document.getElementById("ball");
@@ -27,11 +28,26 @@ export default class Ball {
   update(deltaTime) {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
+    //Wall on left or right
     if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
+    //Wall on top or bottom
     if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
       this.speed.y = -this.speed.y;
+    }
+    //Collision with paddle
+    let ballBottom = this.position.y + this.size;
+    let paddleTop = this.game.paddle.position.y;
+    let paddleLeft = this.game.paddle.position.x;
+    let paddleRight = this.game.paddle.position.x + this.game.paddle.width;
+    if (
+      ballBottom >= paddleTop &&
+      this.position.x >= paddleLeft &&
+      this.position.x + this.size <= paddleRight
+    ) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
     }
   }
 }
